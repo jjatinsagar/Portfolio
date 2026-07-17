3da50665049c8f91e2f9581df5a46580b598b1f7
Jatin Sagar <jjatinsagar@gmail.com>
2026-07-17 20:43:43 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "./css/app.css";
 
 import Screen from "./components/Screen";
 import Navbar from "./components/Navbar";
